2c72f0fd451df02fe5e08f47abb4f5ff981230c1
arzunuvar <62670222+arzunuvar@users.noreply.github.com>
2021-02-03 17:47:00 +0300

    Update app.php

--- app.php
+++ app.php
@@ -9,7 +9,7 @@ $age = 30;
 
 switch ($sex){
 case 'female':
-$calorie= (10 *  $weight) + (6.25 * $height) - (5 * $age) - 161;
+$calorie= (10 * $weight) + (6.25 * $height) - (5 * $age) - 161;
 echo "<p>Your estimated daily metabolic rate is $calorie </p>";
  
 break;
