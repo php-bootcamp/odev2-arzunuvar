d4c560def7eda4a67a9b7f7521ef94d46c69ea7f
arzunuvar <62670222+arzunuvar@users.noreply.github.com>
2021-02-03 17:40:54 +0300

    Update app.php
    Kalori Hesaplama

--- app.php
+++ app.php
@@ -9,12 +9,12 @@ $age = 30;
 
 switch ($sex){
 case 'female':
-$sex= (10 * 60 ) + (6.25 * 170) - (5 * 30) - 161;
+$calorie= (10 *  $weight) + (6.25 * $height) - (5 * $age) - 161;
 echo "<p>Your estimated daily metabolic rate is $sex </p>";
  
 break;
 case 'male':
-$sex= (10 * 55) + (6.25 * 170) - (5 * 25) + 5;
+$calorie= (10 * $weight) + (6.25 * $height) - (5 * $age) + 5;
 echo "<p>Your estimated daily metabolic rate is $sex </p>";
 
 }
