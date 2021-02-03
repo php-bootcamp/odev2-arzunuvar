9553f8246354e918bb1969d564725528361879b3
arzunuvar <62670222+arzunuvar@users.noreply.github.com>
2021-02-03 16:54:34 +0300

    Update app.php
    Kalori Hesaplama

--- app.php
+++ app.php
@@ -14,7 +14,7 @@ echo "<p>Your estimated daily metabolic rate is $sex </p>";
  
 break;
 case 'male':
-$sex= (10 *55) + (6.25 * 170) - (5 * 25) + 5;
+$sex= (10 * 55) + (6.25 * 170) - (5 * 25) + 5;
 echo "<p>Your estimated daily metabolic rate is $sex </p>";
 
 }
