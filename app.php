5fa1fb2e6b6a9374e71b31f5478792c48fcd2e0d
arzunuvar <62670222+arzunuvar@users.noreply.github.com>
2021-02-08 22:46:02 +0300

    Update app.php

--- app.php
+++ app.php
@@ -1,10 +1,27 @@
 <?php 
 
-
 $sex = "female";
 $weight = 60;
 $height = 170;
 $age = 30;
+
+/**
+ * Bu ödevde günlük kalori hesaplaması yapacağız.
+ * Yukarıdaki değişkenleri ve aşağıdaki formülü kullanarak
+ * kişinin günlük kaç kalori alması gerektiğini hesaplayıp
+ * bunu ekrana yazan PHP kodunu oluşturmamız gerekiyor.
+ * 
+ * Erkekler için; 10 x kilo + 6.25 x boy - 5 x yaş + 5
+ * Kadınlar için; 10 x kilo + 6.25 x boy - 5 x yaş - 161
+ * 
+ * Örneğin;
+ * $sex = "female";
+ * $weight = 60;
+ * $height = 170;
+ * $age = 30;
+ * 
+ * Ekrana 1326.5 yazması gerekiyor.
+ */ 
 
 
 switch ($sex){
